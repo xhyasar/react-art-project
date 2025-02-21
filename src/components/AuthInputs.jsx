9c5b0767257d70ed5f4c666c44bd7a019eb880ce
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-21 16:16:25 +0300

    refactor: enhance password label styling based on validation state

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -33,7 +33,7 @@ export default function AuthInputs() {
           />
         </p>
         <p>
-          <label>Password</label>
+          <label className={`label ${emailNotValid ? 'invalid' : ''}`}>Password</label>
           <input
             type="password"
             className={passwordNotValid ? 'invalid' : undefined}
