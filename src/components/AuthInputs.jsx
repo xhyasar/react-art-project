3413bdc8848ea3867304af5c83b47a69747fca86
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-21 15:46:54 +0300

    refactor: improve styling and structure of authentication inputs and header

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -24,9 +24,10 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <div className="controls">
         <p>
-          <label>Email</label>
+          <label className={`label ${emailNotValid ? 'invalid' : ''}`}>Email</label>
           <input
             type="email"
+            //style={{ backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'}}
             className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
