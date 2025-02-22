b04b3fd92808596b4fd61dde5ab6aef42cd2ea7b
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-22 20:02:30 +0300

    fix: prevent rendering of null logo in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,10 @@ import React from "react";
 
 
 export default function Header() {
+    // noinspection JSValidateTypes
     return (
         <header className="flex flex-col items-center mt-8 mb-16 ">
-            <img src={logo ?? null} alt="A canvas" className=" object-contain mb-8 w-44 h-44" />
+            {logo && <img src={logo} alt="A canvas" className=" object-contain mb-8 w-44 h-44" />}
             <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
                 ReactArt
             </h1>
