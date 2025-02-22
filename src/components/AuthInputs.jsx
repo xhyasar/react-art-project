64019ece56a726cddd9bdb2a851dc9d6b8b2e4eb
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-22 12:36:39 +0300

    refactor: implement styled-components for AuthInputs layout and structure

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,54 +1,62 @@
-import { useState } from 'react';
+import {useState} from 'react';
+import {styled} from "styled-components";
+
+const ControlContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    margin-bottom: 1.5rem;
+`
 
 export default function AuthInputs() {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const [submitted, setSubmitted] = useState(false);
+    const [enteredEmail, setEnteredEmail] = useState('');
+    const [enteredPassword, setEnteredPassword] = useState('');
+    const [submitted, setSubmitted] = useState(false);
 
-  function handleInputChange(identifier, value) {
-    if (identifier === 'email') {
-      setEnteredEmail(value);
-    } else {
-      setEnteredPassword(value);
+    function handleInputChange(identifier, value) {
+        if (identifier === 'email') {
+            setEnteredEmail(value);
+        } else {
+            setEnteredPassword(value);
+        }
     }
-  }
 
-  function handleLogin() {
-    setSubmitted(true);
-  }
+    function handleLogin() {
+        setSubmitted(true);
+    }
 
-  const emailNotValid = submitted && !enteredEmail.includes('@');
-  const passwordNotValid = submitted && enteredPassword.trim().length < 6;
+    const emailNotValid = submitted && !enteredEmail.includes('@');
+    const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
-  return (
-    <div id="auth-inputs">
-      <div className="controls">
-        <p>
-          <label className={`label ${emailNotValid ? 'invalid' : ''}`}>Email</label>
-          <input
-            type="email"
-            //style={{ backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'}}
-            className={emailNotValid ? 'invalid' : undefined}
-            onChange={(event) => handleInputChange('email', event.target.value)}
-          />
-        </p>
-        <p>
-          <label className={`label ${emailNotValid ? 'invalid' : ''}`}>Password</label>
-          <input
-            type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
-            onChange={(event) =>
-              handleInputChange('password', event.target.value)
-            }
-          />
-        </p>
-      </div>
-      <div className="actions">
-        <button type="button" className="text-button">
-          Create a new account
-        </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
-      </div>
-    </div>
-  );
+    return (
+        < div id="auth-inputs">
+            <ControlContainer>
+                <p>
+                    <label className={`label ${emailNotValid ? 'invalid' : ''}`}>Email</label>
+                    <input
+                        type="email"
+                        //style={{ backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'}}
+                        className={emailNotValid ? 'invalid' : undefined}
+                        onChange={(event) => handleInputChange('email', event.target.value)}
+                    />
+                </p>
+                <p>
+                    <label className={`label ${emailNotValid ? 'invalid' : ''}`}>Password</label>
+                    <input
+                        type="password"
+                        className={passwordNotValid ? 'invalid' : undefined}
+                        onChange={(event) =>
+                            handleInputChange('password', event.target.value)
+                        }
+                    />
+                </p>
+            </ControlContainer>
+            <div className="actions">
+                <button type="button" className="text-button">
+                    Create a new account
+                </button>
+                <button className='button' onClick={handleLogin}>Sign In</button>
+            </div>
+        </div>
+    );
 }
