efb71050f4cde69a062db82ab3dc2cf714c01296
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-23 17:04:13 +0300

    refactor: convert Button and Input components to use Tailwind CSS for improved styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,9 @@ import React from "react";
 export default function Header() {
     // noinspection JSValidateTypes
     return (
-        <header className="flex flex-col items-center mt-8 mb-16 ">
+        <header className="flex flex-col items-center mt-8 mb-8 md:mb-16 ">
             {logo && <img src={logo} alt="A canvas" className=" object-contain mb-8 w-44 h-44" />}
-            <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
+            <h1 className="text-xl md:text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
                 ReactArt
             </h1>
             <p className="text-stone-500">A community of artists and art-lovers.</p>
